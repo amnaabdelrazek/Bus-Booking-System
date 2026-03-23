7947a072156b8eedfbae6454442251c2309ad8e2
Amna Abdelrazek <amnarazek2003@gmail.com>
2026-03-24 00:22:47 +0200

    Edit Register

--- obj/Debug/net8.0/Bus Booking System.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bus Booking System.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bus Booking System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6577e9086d2883744aeaa3270dc89516982a1e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+790e6f7c2e3588a62a4d83a5e4972cff452ef491")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bus Booking System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bus Booking System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
